cf7052ea9547804facca292e434373e0181ed3d9
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-16 14:13:45 +0200

    Beräkningar för uppgift 2

---
+++ uppgift2.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Programmet räknar ut det totala priset för produkter och pris inkl. moms. 
+ Av Julia Anderberg, 2026 */
+
+// Produktens pris och antal produkter
+const price = 100;
+const quantity = 3;
+
+// Beräkningar
+const sum = price * quantity;
+const sumWithVat = sum * 1.25;
