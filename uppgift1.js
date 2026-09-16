33f94a0bd5eb6c0a93e01387393fd92a4bcd12d7
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-16 13:53:08 +0200

    Skapade variabler och utskrift i uppgift 1

---
+++ uppgift1.js
@@ -0,0 +1,15 @@
+"use strict";
+/* Programmet innehåller personuppgifter och skriver ut informationen.
+ Av Julia Anderberg, 2026 */
+
+// Information om personen
+const firstname = "Julia";
+const lastname = "Anderberg";
+const age = 22;
+const isStudent = true;
+
+// Utskrift 
+console.log(firstname + " " + lastname);
+console.log("Ålder: " + age);
+console.log("Student: " + isStudent);
+
