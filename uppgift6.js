915c23b70d036ecddce8341c06279ac5d98a18ac
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-17 17:38:45 +0200

    Skriver ut resultaten av funktionen i uppgift 6

--- uppgift6.js
+++ uppgift6.js
@@ -1,10 +1,20 @@
 "use strict";
-/* Programmet.
+/* Programmet räknar ut arean av en rektangel tre gånger. 
  Av Julia Anderberg, 2026 */
  
 // Funktion som beräknar arean
 function calculateArea(bredd, höjd) {
     return bredd * höjd; 
 }
+
+// Skriver ut resultaten av funktionen 
+const area1 = calculateArea(80, 40);
+const area2 = calculateArea(100, 80);
+const area3 = calculateArea(300, 150);
+
+console.log("Arean är " + area1);
+console.log("Arean är " + area2);
+console.log("Arean är " + area3);
+
 
 
