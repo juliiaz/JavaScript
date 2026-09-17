ce52a08142c6eb103bcf458848083986e6d8f801
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-17 14:54:59 +0200

    Lade till array, skrev ut första och sista elementen

---
+++ uppgift5.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Programmet.....
+ Av Julia Anderberg, 2026 */
+
+// Array maträtter 
+const maträtter = ["Bolognese", "Tacos", "Paella", "Falafel", "Biryani", "Sushi"];
+    console.log(maträtter);
+
+    console.log(maträtter[0]);
+
+    console.log(maträtter[5]);
