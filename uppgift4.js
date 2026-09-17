f5801f794068aef8127483e24868b66fe0f5e528
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-17 14:30:38 +0200

    Villkor för att skriva ut jämna tal

--- uppgift4.js
+++ uppgift4.js
@@ -1,9 +1,16 @@
 "use strict";
-/* Programmet .....
+/* Programmet skriver ut jämna tal mellan 1 och 20
  Av Julia Anderberg, 2026 */
 
  // For-loop för talen 1 till 20
 
  for (let i = 1; i <= 20; i++) {
+
+ // Villkor för att bara skriva ut jämna tal 
+ if (i % 2 === 0){
+
     console.log(i);
+
+    }
  }
+
