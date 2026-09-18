9150e02214d825bff2ed57f6db538dc36958af38
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-18 13:51:15 +0200

    Skapade funktion och utskrift av bokinformationen i uppgift 8

--- uppgift8.js
+++ uppgift8.js
@@ -1,10 +1,23 @@
 "use strict";
-/* Programmet .  
+/* Programmet visar information om en bok och skriver ut informationen.
  Av Julia Anderberg, 2026 */
 
+// Objekt som representerar en bok
 const bok = {
     titel: "The picture of Dorian Gray", 
     författare: "Oscar Wilde",
     utgivningsår: 1890
     };
+
+// Funktion som tar emot bokobjektet som parametrar 
+function bokInformation(bok) {
+
+    console.log("Titel: " + bok.titel);
+    console.log("Författare: " + bok.författare);
+    console.log("Utgivningsår: " + bok.utgivningsår);
+}
+
+// Utskrift av bokinformation 
+bokInformation(bok);
+
 
