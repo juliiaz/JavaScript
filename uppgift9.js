02ee66758f9b2bdf5528b6fa2ffee1a569883cf4
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-18 14:27:01 +0200

    Skapade array och for-loop för arrayen i uppgift 9

---
+++ uppgift9.js
@@ -0,0 +1,39 @@
+"use strict";
+/* Programmet.
+ Av Julia Anderberg, 2026 */
+
+// Array med tre stycken objekt 
+const people = [
+{
+    name: "Julia",
+    age: 22,
+    city: "Göteborg"
+},
+{
+    name: "Maya",
+    age: 23, 
+    city: "Malmö"
+}, 
+{
+    name: "Jonathan", 
+    age: 25, 
+    city: "Stockholm"
+}
+]; 
+
+// Loop för arrayen
+for (let i = 0 ; i < people.length; i++){
+    const person = people[i];
+}
+
+
+
+
+
+
+
+
+
+
+
+
