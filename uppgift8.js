68dbc5c1a1047c93fe61f003d1eaca3bd80a3de1
Julia Anderberg <julia.anderbeerg222@gmail.com>
2026-09-18 13:37:59 +0200

    Skapade objekt som representerar en bok i uppgift 8

---
+++ uppgift8.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Programmet .  
+ Av Julia Anderberg, 2026 */
+
+const bok = {
+    titel: "The picture of Dorian Gray", 
+    författare: "Oscar Wilde",
+    utgivningsår: 1890
+    };
+
